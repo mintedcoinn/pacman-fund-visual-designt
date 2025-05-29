3820356b661a468de9085e89a4c564ba0ad80a3f
mintedcoinn <mintedcoin120000bc@gmail.com>
2025-05-29 20:20:48 +1000

    New: Static size of maze

--- MyWorld.java
+++ MyWorld.java
@@ -20,19 +20,25 @@ public class MyWorld extends World {
         {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
         {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
     };
-
+    public static int worldPieceSize = 40;
+    public static int worldHalfPieceSize = worldPieceSize /2;
+    public static int POWER_PILL_COUNT = 8;
     public MyWorld() {    
         super(1000, 680, 1);
         buildLab();
+        spawnRed();
     }
 
     private void buildLab() {
         for (int y = 0; y < wrld.length; y++) {
             for (int x = 0; x < wrld[y].length; x++) {
                 if (wrld[y][x] == 1) {
-                    addObject(new Wall(), x * 40 + 20, y * 40 + 20);
+                    addObject(new Wall(), x * worldPieceSize + worldHalfPieceSize, y * worldPieceSize + worldHalfPieceSize);
                 }
             }
         }
+    }
+    private void spawnRed(){
+        addObject(new Red_ghost(), 10*worldPieceSize + worldHalfPieceSize,9*worldPieceSize + worldHalfPieceSize);
     }
 }
