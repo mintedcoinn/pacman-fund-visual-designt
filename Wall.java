1f77a0d48cb3c66a99a8af7ec0a4ad07eae3c6b9
mintedcoinn <mintedcoin120000bc@gmail.com>
2025-06-05 00:18:27 +1000

    nawigation matrix update, walls switch cases update

--- Wall.java
+++ Wall.java
@@ -1,17 +1,24 @@
 import greenfoot.*;
 
 public class Wall extends Actor {
+    GreenfootImage img = new GreenfootImage("background.jpg");
+    static int CELL_SIZE = MyWorld.worldPieceSize;
     public Wall(int n) {
-        GreenfootImage img = new GreenfootImage("block.png");
-        if (n == 2) img = new GreenfootImage("up_round_block.png");
-        if (n == 3) img = new GreenfootImage("down_round_block.png");
-        if (n == 4) img = new GreenfootImage("left_round_block.png");
-        if (n == 5) img = new GreenfootImage("right_round_block.png");
-        if (n == 6) img = new GreenfootImage("left_block.png");
-        if (n == 7) img = new GreenfootImage("right_block.png");
-        if (n == 8) img = new GreenfootImage("down_right_block.png");
-        if (n == 9) img = new GreenfootImage("down_left_block.png");
-        img.scale(20, 20);
+        
+        switch (n){
+        case -1: img = new GreenfootImage("block.png");break;
+        case -2: img = new GreenfootImage("up_round_block.png"); break;
+        case -3: img = new GreenfootImage("down_round_block.png");break;
+        case -4: img = new GreenfootImage("left_round_block.png");break;
+        case -5: img = new GreenfootImage("right_round_block.png");break;
+        case -6: img = new GreenfootImage("left_block.png");break;
+        case -7:img = new GreenfootImage("right_block.png");break;
+        case -8: img = new GreenfootImage("down_right_block.png");break;
+        case -9: img = new GreenfootImage("down_left_block.png");break;
+        default:break;}
+        if (img != null){
+        img.scale(CELL_SIZE, CELL_SIZE);
         setImage(img);
+        }
     }    
 }
